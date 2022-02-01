d39325d85220230f66cbf671f596bf69f09f3fb9
VTramon <vitorr.ooliveira@gmail.com>
2022-02-01 15:31:17 -0300

    created the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 import styles from './style.module.scss'
 import HeaderImg from '../../../public/pic-1.jpg'
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <div className={styles.header}>
       <div>
-        <Image src={HeaderImg} alt="imagem de alimentos saudaveis" />
+        <img src="/pic-1.jpg" alt="imagem de alimentos saudaveis" />
         <h1>Be helthy</h1>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import { Header } from '../components/Header'
 import { Layout } from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout>
-    <Header />
-  </Layout>
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0 " />
+    </Head>
+
+    <Layout>
+      <Header />
+    </Layout>
+  </>
 )
 
 export default IndexPage
